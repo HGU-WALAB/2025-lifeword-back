35bbf1f5798d058014955b31b899f6ebe0acb2f5
naimkim <naimk5451@gmail.com>
2025-01-13 14:53:22 +0900

    FEAT match sermon and content repos

--- src/main/java/com/project/bibly_be/sermon/repo/ContentRepo.java
+++ src/main/java/com/project/bibly_be/sermon/repo/ContentRepo.java
@@ -1,12 +1,10 @@
 package com.project.bibly_be.sermon.repo;
 
+import com.project.bibly_be.sermon.entity.Content;
 import com.project.bibly_be.sermon.entity.Sermon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface SermonRepo extends JpaRepository<Sermon, Long> {
-    List<Sermon> findByIsPublic(Boolean isPublic);
+public interface ContentRepo extends JpaRepository<Content, Long> {
 }
