819b6287fafbb17728bafe7a09fabe61c3fb3a8b
seowon <seowon1112@gmail.com>
2025-01-14 16:00:25 +0900

    UserRequestDTO 필드 추가

--- src/main/java/com/project/bibly_be/user/dto/UserRequestDTO.java
+++ src/main/java/com/project/bibly_be/user/dto/UserRequestDTO.java
@@ -2,8 +2,10 @@ package com.project.bibly_be.user.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class UserRequestDTO {
     private String oauthProvider;  // 예: "kakao", "google", "apple"
@@ -11,6 +13,9 @@ public class UserRequestDTO {
     private String name;
     private String contact;
     private String email;
+    private String pastor;
+    private String place;
+    private String church;
 
 
 }
