c2ed6564b78651b0a058b0920890be99e1f4bb28
Diggydogg <22000471@handong.edu>
2025-01-21 21:43:21 +0900

    Revert "Edit sermonService : add userId (Line 130))"
    This reverts commit c1125c1e08505c937bf5b6e27111a7aa5468b414.

--- src/main/java/com/project/bibly_be/sermon/service/SermonService.java
+++ src/main/java/com/project/bibly_be/sermon/service/SermonService.java
@@ -127,7 +127,6 @@ public class SermonService {
                 .map(sermon -> SermonResponseDTO.builder()
                         .sermonId(sermon.getSermonId())
                         .ownerName(sermon.getOwner().getName())
-                        .userId(sermon.getOwner().getId()) //UUID userId
                         .sermonDate(sermon.getSermonDate())
                         .createdAt(sermon.getCreatedAt())
                         .updatedAt(sermon.getUpdatedAt())
