efef0f37a16a6aadc2415750c66dc6f6229f2f50
Diggydogg <22000471@handong.edu>
2025-03-13 16:46:14 +0900

    last ver

--- src/main/java/com/project/bibly_be/text/repository/TextRepository.java
+++ src/main/java/com/project/bibly_be/text/repository/TextRepository.java
@@ -17,7 +17,7 @@ public interface TextRepository extends JpaRepository<Text, Long> {
     List<Text> findBySermonIdAndVisibility(@Param("sermonId") Long sermonId, @Param("userId") UUID userId);
 
     //Long countBySermon_SermonIdAndIsDraft(Long sermonId, Boolean isDraft);
-    @Query("SELECT COUNT(t) FROM Text t WHERE t.sermon.sermonId = :sermonId AND t.isDraft = false OR  (t.isDraft = true AND t.user.id = :userId)")
+    @Query("SELECT COUNT(t) FROM Text t WHERE t.sermon.sermonId = :sermonId AND (t.isDraft = false OR (t.isDraft = true AND t.user.id = :userId))")
     Long countBySermonIdWithIsDraft(@Param("sermonId") Long sermonId, @Param("userId") UUID userId);
 
 
